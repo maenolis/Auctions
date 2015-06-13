f07c51515234120b2d0d211897171020605e9572
maenolis <psimukas@hotmail.com>
2015-06-14 00:35:30 +0300

    Testing Moxy.

--- auctions/src/main/java/org/maenolis/auctions/services/LoginService.java
+++ auctions/src/main/java/org/maenolis/auctions/services/LoginService.java
@@ -1,35 +1,27 @@
 package org.maenolis.auctions.services;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlSeeAlso;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.maenolis.auctions.dao.User;
 
+
+@XmlSeeAlso({User.class})
 @Path("/Login")
 public class LoginService {
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response login(@Context HttpServletRequest request,
-			@Context HttpServletResponse response) throws IOException {
-		StringBuffer sb = new StringBuffer();
-		BufferedReader reader = request.getReader();
-		String line = null;
-		while ((line = reader.readLine()) != null) {
-			sb.append(line);
-		}
-		ObjectMapper mapper = new ObjectMapper();
-		return Response.status(201).entity("allrighty").build();
+	public User login(User user) throws IOException {
+		
+		System.out.println(user.getEmail() + " " + user.getPassword());
+		return user;
 	}
 }
