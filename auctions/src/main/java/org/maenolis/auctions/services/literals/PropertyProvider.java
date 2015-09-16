94f68caa3c3f20287326520a2ef4f24a7b571aec
maenolis <psimukas@hotmail.com>
2015-09-16 20:28:48 +0300

    New helper class.

---
+++ auctions/src/main/java/org/maenolis/auctions/services/literals/PropertyProvider.java
@@ -0,0 +1,19 @@
+package org.maenolis.auctions.services.literals;
+
+public class PropertyProvider {
+
+	public static final String STATUS = "status";
+
+	public static final String OK = "ok";
+
+	public static final String NOK = "nok";
+
+	public static final String USERNAME = "username";
+
+	public static final String MESSAGES = "messages";
+
+	public static final String AUCTIONS = "auctions";
+
+	public static final String USERID = "userId";
+
+}
