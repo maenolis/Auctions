8496950346edc04ba41f2a5a82277c052bbddb7b
maenolis <psimukas@hotmail.com>
2015-10-04 14:59:49 +0300

    Xml fixed.

--- auctions/src/main/java/org/maenolis/auctions/services/AdminService.java
+++ auctions/src/main/java/org/maenolis/auctions/services/AdminService.java
@@ -1,6 +1,5 @@
 package org.maenolis.auctions.services;
 
-import java.io.StringWriter;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,11 +8,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 
 import org.maenolis.auctions.dao.Auction;
 import org.maenolis.auctions.dao.User;
@@ -43,35 +40,17 @@ public class AdminService {
 
 	}
 
-	@GET
 	@Path("/auctions")
+	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public Response getAll() throws JAXBException {
+	public Response getAll() {
 
 		List<AuctionRetObject> list = Auction.getAllAuctions();
 
-		if (list.size() == 0) {
-			return Response.status(200).entity("").build();
-		}
+		GenericEntity<List<AuctionRetObject>> ret = new GenericEntity<List<AuctionRetObject>>(
+				list) {
+		};
 
-		JAXBContext jaxbContext = JAXBContext
-				.newInstance(AuctionRetObject.class);
-		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		jaxbMarshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
-		StringBuilder sb = new StringBuilder();
-		StringWriter sw = new StringWriter();
-		sb.append("<auctions>");
-		for (AuctionRetObject auction : list) {
-			jaxbMarshaller.marshal(auction, sw);
-			sb.append(sw.toString());
-			sw.flush();
-		}
-
-		System.out.println("Admin!!!" + " " + sw.toString());
-
-		sb.append("</auctions>");
-		String output = sb.toString();
-		return Response.status(200).entity(output).build();
+		return Response.ok(ret).build();
 	}
 }
